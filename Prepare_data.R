e20c3a453245d427e54acbef63e751f592c7eca5
MartinBulla <MartinBulla>
2019-01-18 09:07:18 +0100

    TABLE_S1 finalized, rest will be moved to a new Script
    prepare data - AIC option TRUE/False + added percentages

--- Prepare_data.R
+++ Prepare_data.R
@@ -3,13 +3,58 @@ trees = read.tree(paste(wd,"trees2.phy",sep=""))
 trees1 = lapply(trees, function( t ) addInTip( t , "Charadrius_alexandrinus","Charadrius_nivosus" )  )
 trees2 = lapply( trees1, function( t ) addInTip( t , "Gallinago_gallinago", "Gallinago_delicata") ) 
 
-# load and prepare predation data
-d = read.csv(paste(wd,"DATApopulations.csv",sep=""), h = T, sep=";")
+# load citation data
+n = readWorksheetFromFile(paste(wd, 'literature_check.xlsx',sep = ""), colTypes = 'character', sheet = "Data")	
+n = n[,c("ref_num","abb_ref","locality")]
+
+# load and prepare data later shared (and added to Dryad) by Kubelka - we have split the 'species' column into two (used in Science = species, and other name species2)
+b = readWorksheetFromFile(paste(wd, 'Kubelka et al._2018_Science_additional datafile_1.xlsx',sep = ""), colTypes = 'character', sheet = "DATA")	
+b = b[order(b$species,b$Latitude, b$mean_year),]
+
+ # assign citation number
+ b$source_id = NA
+ b$source_id = as.character(b$source_id)
+ for(i in 1:nrow(n)){
+	#i = 1
+	ni = n[i,]
+	b$source_id = ifelse(grepl(ni$abb_ref, b$References.and.notes),ifelse(is.na(b$source_id), ni$ref_num, paste(b$source_id, ni$ref_num, sep=',')), b$source_id)
+	print(ni$abb_ref)
+	}
+#b[is.na(b$source_id),]	
+
+b$DPRtrans[b$pk == 23]='NO' # correct a typo - the estimate for this nest was not transformed by Kubelka et al 
+b$obs_time = as.numeric(b$obs_time)
+b$Incubation_days = as.numeric(b$Incubation_days)
+b$predated = as.numeric(b$predated)
+b$hatched = as.numeric(b$hatched)
+b$infertile = as.numeric(b$infertile)
+b$Latitude = as.numeric(b$Latitude)
+b$mean_year = as.numeric(b$mean_year)
+b$DPR_orig = as.numeric(b$DPR_orig)
+b$Exposure_days = as.numeric(b$Exposure_days)
+b$N_nests = as.numeric(b$N.nests)
+b$"Failed_together." = as.numeric(b$"Failed_together.")
+
+
+b$site = paste(b$Latitude,b$Longitude) # define site
+b$lat_abs = abs(b$Latitude) # abs latitude
+b$ln_N_nests = log(b$N_nests)
+b$hemisphere =as.factor(ifelse(b$Latitude > 0, "Northern", "Southern"))
+b$genus = gsub("\\_.*","",b$species)
+	
+# load and prepare predation data used in the paper
+d = read.csv(paste(wd,"DATApopulations.csv",sep=""), h = T, sep=";",stringsAsFactors = FALSE)
+d$source_id = b$source_id[match(paste(d$species, d$mean_year, round(d$Latitude,2)),paste(b$species, b$mean_year, round(b$Latitude,2)))] 
+#d[is.na(d$source_id),c('species','mean_year','Latitude')]
+#d[is.na(d$source_id),]
+#x = d$species[is.na(d$source_id)]
+#b[b$species%in%x,c('species','mean_year','Latitude')]
 d$site = paste(d$Latitude,d$Longitude) # define site
 d$lat_abs = abs(d$Latitude) # abs latitude
 d$ln_N_nests = log(d$N_nests)
 d$hemisphere =as.factor(ifelse(d$Latitude > 0, "Northern", "Southern"))
 d$genus = gsub("\\_.*","",d$species)
+
 # prepare phylogenetic, distance and PI matricies
 tree2 = trees2[[42]]
 phyloMat <- vcv.phylo( tree2 )
--- Sensitivity_Analyses.R
+++ Sensitivity_Analyses.R
@@ -1,7 +1,7 @@
 rm( list = ls() )	
 # set working and output directories
-wd = 'C:/Users/mbulla/Documents/Dropbox/Science/Projects/MS/Kubelka_et_al_rebuttal/'
-outdir = 'C:/Users/mbulla/Documents/Dropbox/Science/Projects/MS/Kubelka_et_al_rebuttal/'
+wd = 'C:/Users/mbulla/Documents/Dropbox/Science/Projects/MS/Kubelka_et_al_rebuttal/Analyses/'
+outdir = 'C:/Users/mbulla/Documents/Dropbox/Science/Projects/MS/Kubelka_et_al_rebuttal/Outputs/'
 
 # print figures in PNG or not
 PNG = TRUE
@@ -10,26 +10,9 @@ PNG = TRUE
 		source(paste(wd, 'Constants_Functions.R',sep=""))
 		source(paste(wd, 'Prepare_Data.R',sep="")) # generates 18 warnings, same way as Kubelke et al's script
 
-# prepare
-b = readWorksheetFromFile(paste(wd, 'additional data/Kubelka et al._2018_Science_additional datafile_1.xlsx',sep = ""), colTypes = 'character', sheet = "DATA")	
-b = b[order(b$species,b$Latitude, b$mean_year),]	
-b$DPRtrans[b$pk == 23]='NO' # correct a typo - the estimate for this nest was not transformed by Kubelka et al 
-b$obs_time = as.numeric(b$obs_time)
-b$Incubation_days = as.numeric(b$Incubation_days)
-b$predated = as.numeric(b$predated)
-b$hatched = as.numeric(b$hatched)
-b$infertile = as.numeric(b$infertile)
-b$Latitude = as.numeric(b$Latitude)
-b$mean_year = as.numeric(b$mean_year)
-b$DPR_orig = as.numeric(b$DPR_orig)
-b$Exposure_days = as.numeric(b$Exposure_days)
-b$N.nests = as.numeric(b$N.nests)
-b$"Failed_together." = as.numeric(b$"Failed_together.")
-b$ln_N_nests = log(b$N.nests)
-
 cols = brewer.pal(n = 9, name = "Blues")	
-
-# all years
+b$other_failed0 = ifelse(is.na(b$other_failed),0,b$other_failed)
+{# all years
 if(PNG == TRUE) {
 		png(paste(outdir,"sensitivity_anal.png", sep=""), width=1.85,height=1.85,units="in",res=600)	 
 		#	png(paste(outdir,"Figure_1a_Arc_temp.png", sep=""), width=1.85,height=1.85,units="in",res=600)	 
@@ -76,8 +59,8 @@ for(i in 1: length(seq(0.1,0.9, by = 0.1))){
 print(i)
 	}
 if(PNG == TRUE) {dev.off()}
-
-# >1970
+}
+{# >1970
 if(PNG == TRUE) {
 		png(paste(outdir,"sensitivity_anal_1970+.png", sep=""), width=1.85,height=1.85,units="in",res=600)	 
 		#	png(paste(outdir,"Figure_1a_Arc_temp.png", sep=""), width=1.85,height=1.85,units="in",res=600)	 
@@ -126,8 +109,8 @@ for(i in 1: length(seq(0.1,0.9, by = 0.1))){
 print(i)
 	}
 if(PNG == TRUE) {dev.off()}
-
-# <2000
+}
+{# <2000
 if(PNG == TRUE) {
 		png(paste(outdir,"sensitivity_anal_less2000.png", sep=""), width=1.85,height=1.85,units="in",res=600)	 
 		#	png(paste(outdir,"Figure_1a_Arc_temp.png", sep=""), width=1.85,height=1.85,units="in",res=600)	 
@@ -175,3 +158,62 @@ for(i in 1: length(seq(0.1,0.9, by = 0.1))){
 print(i)
 	}
 if(PNG == TRUE) {dev.off()}		
+}
+# using only data with exposure from literature
+bb =b[which(b$DPRtrans=='NO'),]
+m = lmer(log(DPR_orig+0.01) ~ ln_N_nests + mean_year + (1|mean_year) +(1|Latitude)+(1|species),  data = bb)  
+plot(allEffects(m))
+summary(glht(m))
+
+b[b$mean_year<1970,'References.and.notes']
+b[b$mean_year<1970,'Locality']
+
+{## all years - adjusting for all data (not juse <2000) # only with 0.3 starts to be significant
+if(PNG == TRUE) {
+		png(paste(outdir,"sensitivity_anal_all.png", sep=""), width=1.85,height=1.85,units="in",res=600)	 
+		#	png(paste(outdir,"Figure_1a_Arc_temp.png", sep=""), width=1.85,height=1.85,units="in",res=600)	 
+					#jpeg(paste(outdir,"Figure_3.jpeg", sep=""), width=3.5,height=1.85,units="in",res=600) 
+					}else{
+					dev.new(width=1.85,height=1.85) #dev.new(width=3.5, height=1.97)
+					}	
+par(mar=c(2.2,1.7,0.5,0.5), ps=12, mgp=c(1.2,0.35,0), las=1, cex.lab=0.6,cex.main=0.7, cex.axis=0.5, tcl=-0.15,bty="n",xpd=TRUE, col.axis="black",font.main = 1, col.lab="black", col.main="black", fg="black", lwd = 0.5)
+plot(b$DPR_orig~b$mean_year, pch=19,xlim=c(1944,2016), ylim=c(0,0.1),ylab=NA,xlab=NA, xaxt='n',yaxt='n', type='n')
+axis(1, at=c(1944,1960,1980,2000,2016),labels=c(1944,1960,1980,2000,2016),cex.axis=0.5,mgp=c(0,-0.15,0), lwd = 0.5)
+			mtext('Year',side=1,line=0.6, cex=0.6, las=1, col='grey30')
+axis(2, at=seq(0,0.1, by = 0.01), labels=seq(0,0.1, by = 0.01), lwd = 0.5)
+			mtext('Daily nest predation',side=2,line=1.1, cex=0.6, las=3, col='grey30')
+text(x = c(1980,2000, 2006, 2010), y =0.1, labels= c('Beintema:','0.1','-','0.9'), col=c('darkgrey',cols[2], 'darkgrey', cols[9]), cex = 0.5, adj = 0)			
+for(i in 1: length(seq(0.1,0.9, by = 0.1))){
+ #i=2
+ r = seq(0.1,0.9, by = 0.1)[i]
+ b$expMB_r = (r*b$Incubation_days*(b$predated+b$other_failed0)/2)+(r*b$Incubation_days*(b$hatched+b$infertile))
+ b$expMB_r = ifelse(grepl('Moit',b$'References.and.notes'),(r*b$Incubation_days*b$"Failed_together."/2)+(r*b$Incubation_days*(b$hatched+b$infertile)), b$expMB_r)
+ b$expMB_r = ifelse(grepl('Favero',b$'References.and.notes'),(r*b$Incubation_days*b$"Failed_together."/2)+(r*b$Incubation_days*(b$hatched+b$infertile)), b$expMB_r)
+ b$DPR_MB_r = b$predated/b$expMB_r
+ b$DPR = ifelse(b$DPRtrans=="NO", b$DPR_orig,  b$DPR_MB_r)
+ 
+ m = lmer(log(DPR+0.01) ~ ln_N_nests + mean_year + (1|mean_year) +(1|Latitude)+(1|species),  data = b)  
+ #plot(allEffects(m))
+ #summary(glht(m))
+ bsim <- sim(m, n.sim=nsim)  
+ v = apply(bsim@fixef, 2, quantile, prob=c(0.5))
+ # values to predict for		
+   newD=data.frame(ln_N_nests = mean(b$ln_N_nests),mean_year = seq(min(b$mean_year),max(b$mean_year), length.out=300))
+ # exactly the model which was used has to be specified here 
+   X <- model.matrix(~ ln_N_nests + mean_year,data=newD)	
+ # calculate predicted values and creditability intervals
+   newD$pred <- X%*%v # #newD$fit_b <- plogis(X%*%v) # in case on binomial scaleback
+   predmatrix <- matrix(nrow=nrow(newD), ncol=nsim)
+   for(j in 1:nsim) predmatrix[,j] <- X%*%bsim@fixef[j,]
+					newD$lwr <- apply(predmatrix, 1, quantile, prob=0.025)
+					newD$upr <- apply(predmatrix, 1, quantile, prob=0.975)
+	
+		ppi = newD
+		polygon(c(ppi$mean_year, rev(ppi$mean_year)), c(exp(ppi$lwr)-0.01, 
+		  rev(exp(ppi$upr)-0.01)), border=NA, col = adjustcolor(cols[i],alpha.f = 0.2))#adjustcolor(col_t ,alpha.f = 0.2)) #0,0,0 black 0.5 is transparents RED
+		lines(ppi$mean_year, exp(ppi$pred)-0.01, col=cols[i],lwd=1)
+print(i)
+	}
+if(PNG == TRUE) {dev.off()}
+
+}
